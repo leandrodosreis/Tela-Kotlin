19f4424a0e73f29d16a4eff8cfbbc7cda469de8d
LEANDRO DOS REIS FILHO <25203700@dsn127m1246734.cfp127aluno.local>
2026-09-15 15:37:35 -0300

    testando orientaçao

--- app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -5,18 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,15 +26,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLayoutsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier
+
+                    LayoutScreen(modifier = Modifier
                         .padding(innerPadding)
-                        .background(Color.Blue).size(
-                            width = 200.dp,
-                            height = 300.dp
-                        ),
-                        contentAlignment = Alignment.Center){
-                        Text("Aula android")
-                    }
+                    )
+
+//                    Box(modifier = Modifier
+//                        .padding(innerPadding)
+//                        .background(color = Color.Blue)
+//                        .size(
+//                            width = 200.dp,
+//                            height = 300.dp
+//                        ),
+//                        contentAlignment = Alignment.TopCenter){
+//                        Text("Aula android")
+//                        Text("Com Jetpack Compose",
+//                            modifier = Modifier.align(Alignment.BottomCenter))
+//                    }
                 }
             }
         }
@@ -42,17 +50,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LayoutScreen (modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.background(Color.Red).fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column( Modifier.background(Color.Blue)
+            .weight(1f)
+            .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeLayoutsTheme {
-        Greeting("Android")
+            Text("123")
+            Text("456")
+            Text("789")
+        }
+
+        Row( Modifier.background(Color.Cyan)
+            .weight(1f)
+            .fillMaxWidth(),
+            Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically) {
+
+            Text("123")
+            Text("456")
+            Text("789")
+        }
+
     }
+
 }
